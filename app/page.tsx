1fe87e96e76a4b5e3c67aa5d90601a5fc0afab63
abd-ulbasit <basit01035@gmail.com>
2023-06-15 17:59:31 +0500

    add background gradient

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,18 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white">
-      <h1 className='text-5xl font-mono ' >
-        Hello World! I am
-        <span className=' decoration-4 ml-4 font-semibold underline text-orange-600 italic bg-purple-200 px-2 rounded-sm font-sans decoration-lime-600 decoration-dotted' >
-          Abdul Basit
-        </span>
-      </h1>
-      <p className='italic'>And You are blinded by the light theme</p>
+    <main className="min-h-screen  bg-gradient-conic from-gray-900 to-gray-800 px-1 text-white">
+
+      <div className='flex flex-col items-center  min-h-screen px-0 sm:px-12 py-4 md:px-24 ' >
+        <h1 className='text-5xl font-mono px-auto py-8' >
+          Hello World! I am
+          <span className='inline-block md:inline decoration-4  font-semibold underline text-orange-600 italic bg-purple-200 px-2 rounded-sm font-sans decoration-lime-600 decoration-dotted' >
+            Abdul Basit
+          </span>
+        </h1>
+        <p className='text-2xl text-center' >Studing Computer Science  <span className='text-yellow-400 block ' >&</span> building Quality Web-Applications</p>
+
+      </div>
     </main>
   )
 }
