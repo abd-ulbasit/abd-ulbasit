affdb941e58cee8823da7d7b6c15390d047d4c09
abd-ulbasit <basit01035@gmail.com>
2023-06-16 19:01:33 +0500

    add social Links

---
+++ app/components/Socials.tsx
@@ -0,0 +1,24 @@
+'use client'
+import type { IconType } from "react-icons"
+import { FaEnvelope, FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa"
+function Socials() {
+  //make an array socials that has opjects with two properties one is src one is icon that is of IconType
+  interface social { src: string, icon: IconType }
+  const socials: social[] = [{ src: "www.github.com/abd-ulbasit", icon: FaGithub }, { src: "www.linkedin.com/in/abd-ulbasit", icon: FaLinkedin }, { src: "www.twitter.com/abd_lbasit", icon: FaTwitter }, { src: "mailto:basit01035@gmail.com", icon: FaEnvelope }]
+  return <span
+    className="inline-flex -space-x-px overflow-hidden rounded-md border bg-white shadow-sm dark:border-gray-800 dark:bg-gray-900"
+  >{socials.map((social) => {
+
+    return <button key={social.src} onClick={(e) => { e.preventDefault(); window.open(`https://${social.src}`, "_blank") }}
+      className="inline-block px-8 py-2  font-medium text-gray-700 hover:bg-gray-50 focus:relative dark:text-gray-200 dark:hover:bg-gray-800"
+    >
+      {<social.icon></social.icon>}
+    </button>
+  })
+
+    }
+
+  </span>
+
+}
+export default Socials;
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,24 @@
-
+import Socials from "./components/Socials"
 export default function Home() {
   return (
     <main className="min-h-screen  ">
 
-      <div className='flex flex-col items-center  min-h-screen px-0 sm:px-12 py-4 md:px-24 ' >
-        <h1 className='text-5xl font-mono px-auto py-8' >
-          Hello World! I am
-          <span className='inline-block md:inline decoration-4  font-semibold underline text-orange-600 italic bg-purple-200 px-2 rounded-sm font-sans decoration-lime-600 decoration-dotted' >
-            Abdul Basit
-          </span>
-        </h1>
-        <p className='text-2xl text-center' >Studing Computer Science  <span className='text-yellow-400 block ' >&</span> building Quality Web-Applications</p>
+      <div className='flex flex-col items-center justify-between  min-h-screen px-0 sm:px-12 py-4 md:px-24 ' >
+        <div className="flex flex-col gap-4">
 
+          <h1 className='text-5xl  px-auto py-8' >
+            <span className="block font-bold" >
+              Hello World!
+            </span>
+            I am
+            <span className='inline-block md:inline decoration-4  font-semibold underline text-orange-600 italic  px-2 rounded-sm font-sans   text-6xl' >
+              Abdul Basit
+            </span>
+          </h1>
+          <p className='text-2xl text-center' >Studing Computer Science  <span className='text-yellow-400 block ' >&</span> building Quality Web Applications</p>
+          <p className="italic pt-4" >Together, we&apos;ll bring your ideas to life</p>
+        </div>
+        <Socials></Socials>
       </div>
     </main>
   )
