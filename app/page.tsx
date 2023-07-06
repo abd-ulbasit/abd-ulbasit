12f234a424b1760acdb11e077887f1f5134cc6db
abd-ulbasit <basit01035@gmail.com>
2023-07-07 01:10:12 +0500

    add minimal discription

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,13 @@ import Socials from "./components/Socials"
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center">
-      <div>
-        <h1 className='' >
+      <div className="max-w-sm flex flex-col gap-4">
 
-          I am Abdul Basit
-
-        </h1>
-        <p className='' >Studing Computer Science  </p>
-        <Socials></Socials>
+        <h1 className='text-4xl font-bold font-serif' >Abdul Basit</h1>
+        <p className='italic indent-8' >I study Computer Science. I am currently building applications in NEXTJS, t3-stack and  SvelteKit. I also do leetcode, system design and Rust.</p>
+        <div className="mx-auto" >
+          <Socials></Socials>
+        </div>
       </div>
     </main>
   )
