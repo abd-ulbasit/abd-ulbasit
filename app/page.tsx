dd390b4325640b1b0c59cd9f2b339b21c263e384
abd-ulbasit <basit01035@gmail.com>
2023-07-07 13:59:13 +0500

    add padding to name for small screens

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
     <main className="min-h-screen flex flex-col items-center justify-center">
       <div className="max-w-sm flex flex-col gap-4">
 
-        <h1 className='text-4xl font-bold font-serif' >Abdul Basit</h1>
-        <p className='italic indent-8' >I study Computer Science. I am currently building applications in NEXTJS, t3-stack and  SvelteKit. I also do leetcode, system design and Rust.</p>
+        <h1 className='text-4xl font-bold font-serif pl-2 sm:pl-0' >Abdul Basit</h1>
+        <p className='italic indent-8 ' >I study Computer Science. I am currently building applications in NEXTJS, t3-stack and  SvelteKit. I also do leetcode, system design and Rust.</p>
         <div className="mx-auto" >
           <Socials></Socials>
         </div>
