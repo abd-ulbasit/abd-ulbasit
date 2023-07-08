718b64a26a54d44dabbb1ba6edeb4440db9522a6
abd-ulbasit <basit01035@gmail.com>
2023-07-08 16:59:47 +0500

    add animation on hover to name

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
     <main className="min-h-screen flex flex-col items-center justify-center">
       <div className="max-w-sm flex flex-col gap-4">
 
-        <h1 className='text-4xl font-bold font-serif pl-2 sm:pl-0' >Abdul Basit</h1>
+        <h1 className='text-4xl font-bold font-serif pl-2 sm:pl-0 hover:animate-pulse' >Abdul Basit</h1>
         <p className='italic indent-8  pl-1 sm:pl-0 sm:tracking-normal tracking-tighter' >I study Computer Science. I am currently building applications in NEXTJS, t3-stack and  SvelteKit. I also do leetcode, system design and Rust.</p>
         <div className="mx-auto" >
           <Socials></Socials>
