0c462ee2d0a88db41d9f0aa4fb43ddc0fa8372dd
abd-ulbasit <basit01035@gmail.com>
2023-07-12 23:13:16 +0500

    remove svelte/add AOC

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
       <div className="max-w-sm flex flex-col gap-4">
 
         <h1 className='text-4xl font-bold font-serif pl-2 sm:pl-0 hover:animate-pulse' >Abdul Basit</h1>
-        <p className='italic indent-8  pl-1 sm:pl-0 sm:tracking-normal tracking-tighter' >I study Computer Science. I am currently building applications in NEXTJS, t3-stack and  SvelteKit. I also do leetcode, system design and Rust.</p>
+        <p className='italic indent-8  pl-1 sm:pl-0 sm:tracking-normal tracking-tighter' >I study Computer Science. I am currently building applications in NEXTJS, and t3-stack . I also do Rust, system design, leetcode and advent of code.</p>
         <div className="mx-auto" >
           <Socials></Socials>
         </div>
